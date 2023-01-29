bd1740090d0ddb83e2e8888aa39f03347b0f0995
robert-f-ruff <robert.f.ruff@gmail.com>
2023-01-28 17:05:50 -0800

    Add Models to admin Site Data in these models is maintained by an administrator, not the end user.

--- rules_engine/rules/admin.py
+++ rules_engine/rules/admin.py
@@ -1,3 +1,19 @@
+"""Define the models that are managed by the admin site."""
 from django.contrib import admin
+from .models import Criterion, Parameter, Action, ActionParameters
 
-# Register your models here.
+admin.site.register([Criterion, Parameter,])
+
+
+class ActionParametersInline(admin.TabularInline):
+    """Display the ActionParameters model inline as a table."""
+    model = ActionParameters
+    extra = 1
+
+
+class ActionAdmin(admin.ModelAdmin):
+    """Define view for the Action model."""
+    inlines = (ActionParametersInline,)
+
+
+admin.site.register(Action, ActionAdmin)
