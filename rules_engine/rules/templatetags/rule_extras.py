69664819b53d5df892a4959d97b710eab6d0a3f9
robert-f-ruff <robert.f.ruff@gmail.com>
2023-03-03 18:41:02 -0800

    Add initial set of pages Adding the following pages: *Index Page: display list of rules *Rule Add/Edit Page *Rule Delete Page

---
+++ rules_engine/rules/templatetags/rule_extras.py
@@ -0,0 +1,28 @@
+"""Define custom template filters."""
+import re
+from django.template.defaulttags import register
+from django.template.defaultfilters import stringfilter
+
+@register.filter(is_safe=True)
+@stringfilter
+def get_id_number(id_string):
+    """ This function will extract the id number from the given
+        auto id string.
+    """
+    match = re.search(r'-(\d{1,})-', id_string)
+    if match:
+        return match.group(1)
+    return ''
+
+@register.filter(is_safe=True)
+def get_form_type(form):
+    """ This function will return the type of the given form.
+    """
+    return type(form).__name__
+
+@register.filter(is_safe=True)
+def add_class(control, class_name):
+    """ This function will add the specified css class to the
+        control's list of css classes.
+    """
+    return control.as_widget(attrs={'class': control.css_classes(class_name)})
