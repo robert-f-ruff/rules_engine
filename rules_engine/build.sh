6ee77481985454d3de9b889c6d0ced7540135dda
robert-f-ruff <robert.f.ruff@gmail.com>
2024-04-25 13:09:19 -0700

    Rename compose.yml File to docker-compose-service.yml Giving the compose file a structured name in preparation for creating a development compose file.

--- rules_engine/build.sh
+++ rules_engine/build.sh
@@ -3,4 +3,4 @@
 #Create the Docker images
 echo "Building the user web portal..."
 python3 manage.py collectstatic --noinput
-docker compose -f compose.yml up --build
+docker compose -f docker-compose-service.yml up --build
