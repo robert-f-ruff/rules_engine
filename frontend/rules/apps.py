8c3876edc368acd7d384ee46a9f3bd9c856ce16e
robert-f-ruff <robert.f.ruff@gmail.com>
2024-04-26 23:23:32 -0700

    Cleanup Pylint Issues

--- frontend/rules/apps.py
+++ frontend/rules/apps.py
@@ -1,6 +1,8 @@
+"""Define the rules application."""
 from django.apps import AppConfig
 
 
 class RulesConfig(AppConfig):
+    """The rules application definition."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'rules'
