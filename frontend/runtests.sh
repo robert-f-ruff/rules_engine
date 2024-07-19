f523ed6921aed6009c418371a6325b385f3eb39c
robert-f-ruff <robert.f.ruff@gmail.com>
2024-07-18 19:15:55 -0700

    Streamline Testing *Added code to delete the Python virtual environment to ensure that dependencies of dependencies are at the latest version. *Added code to upgrade pip in the virtual environment. *Removed containerpause.py from the code coverage report as this script is not directly unit tested nor is it central to the application.

--- frontend/runtests.sh
+++ frontend/runtests.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 #Setup the virtual environment
+if [ -d ".venv" ]; then
+  rm -Rf .venv/
+fi
 python3 -m venv .venv
 source .venv/bin/activate
+python3 -m pip install --upgrade pip
 python3 -m pip install -r test_requirements.txt
 
 #Run the tests
-coverage run --source='.' --omit='manage.py','admin.py','apps.py','rules/migrations/*','rules/tests/*','rules_engine/*' runtests.py
+coverage run --source='.' --omit='containerpause.py','manage.py','admin.py','apps.py','rules/migrations/*','rules/tests/*','rules_engine/*' runtests.py
 coverage html
 
 #Shutdown the virtual environment
